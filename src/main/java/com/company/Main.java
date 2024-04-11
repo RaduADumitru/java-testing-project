01afbaafeabb2d2b2f9a165d184a2e592b3727cf
RaduADumitru <radu.dumitru@rocketmail.com>
2024-04-11 21:08:17 +0300

    Replaced class with SecureEquationUtils

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,18 +7,6 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        RouteParser parser = new RouteParser();
-        parser.addRouteWithParams("/users/{}/posts/{}", "/users/(\\d+)/posts/(\\d+)", Arrays.asList("userId", "postId"));
-        parser.addRouteWithParams("/books/{}/chapters/{}", "/books/(\\d+)/chapters/(\\d+)", Arrays.asList("bookId", "chapterId"));
-
-        String url = "/users/123/posts/456";
-        List<String> paramNames = Arrays.asList("userId", "postId");
-        Map<String, String> params = parser.parseWithParams(url, paramNames);
-        if (params != null) {
-            System.out.println("Parsed parameters:");
-            params.forEach((key, value) -> System.out.println(key + ": " + value));
-        } else {
-            System.out.println("No matching route found for " + url);
-        }
+        System.out.println("This main method is a placeholder. The aim of the project is testing of the class SecureEquationUtils.");
     }
 }
