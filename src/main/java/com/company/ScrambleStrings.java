eb67109ddaa917cadf0e82e112f1238680d9f78a
nubaz <lucapaulf@gmail.com>
2024-05-07 23:53:42 +0300

    Instruction indexing for ScrambleStrings

--- src/main/java/com/company/ScrambleStrings.java
+++ src/main/java/com/company/ScrambleStrings.java
@@ -9,54 +9,56 @@ public class ScrambleStrings {
         int j, length;
         char letter;
 
-        if (right < 0 || left < 0)
-            throw new IllegalArgumentException("One of the interval bounds is negative.");
+        if (right < 0 || left < 0) // 1
+            throw new IllegalArgumentException("One of the interval bounds is negative."); // 2
 
-        if (right <= left)
-            throw new IllegalArgumentException("Right interval bound is less than equal to the left one.");
+        if (right <= left) // 3
+            throw new IllegalArgumentException("Right interval bound is less than equal to the left one."); // 4
 
-        if (strings == null)
-            throw new NullPointerException("String list is null.");
-        if (strings.isEmpty())
-            throw new IllegalArgumentException("String list is empty.");
+        if (strings == null) // 5
+            throw new NullPointerException("String list is null."); // 6
+        if (strings.isEmpty()) // 7
+            throw new IllegalArgumentException("String list is empty."); // 8
 
-        for (String string : strings) {
+        for (String string : strings) { // 9
             StringBuilder word = new StringBuilder(string);
-            length = word.length();
+            length = word.length(); // 10
 
-            if (length >= left && length <= right) {
-                for (j = 0; j < length; j++) {
-                    letter = string.charAt(j);
+            if (length >= left && length <= right) { // 11
+                for (j = 0; j < length; j++) { // 12
+                    letter = string.charAt(j); // 13
 
-                    if(Character.isDigit(letter)) {
-                        if((letter - '0') % 2 == 0)
-                            word.setCharAt(j, '$');
-                        else
-                            word.setCharAt(j, '&');
-                    } else {
-                        if ((letter >= 'a' && letter <= 'm') || (letter >= 'A' && letter <= 'M')) letter += 13;
-                        else letter -= 13;
+                    if(Character.isDigit(letter)) { // 14
+                        if((letter - '0') % 2 == 0) // 15
+                            word.setCharAt(j, '$'); // 16
+                        else // 17
+                            word.setCharAt(j, '&'); // 18
+                    } else { // 19
+                        if ((letter >= 'a' && letter <= 'm') || (letter >= 'A' && letter <= 'M')) // 20
+                            letter += 13; // 21
+                        else // 22
+                            letter -= 13; // 23
 
-                        word.setCharAt(j, letter);
+                        word.setCharAt(j, letter); // 24
                     }
                 }
-            } else if (length > right) {
-                j = 0;
+            } else if (length > right) { // 25
+                j = 0; // 26
 
-                do {
-                    if (j % 2 == 0)
-                        word.setCharAt(j, Character.toLowerCase(word.charAt(j)));
-                    else
-                        word.setCharAt(j, Character.toUpperCase(word.charAt(j)));
+                do { // 27
+                    if (j % 2 == 0) // 28
+                        word.setCharAt(j, Character.toLowerCase(word.charAt(j))); // 29
+                    else // 30
+                        word.setCharAt(j, Character.toUpperCase(word.charAt(j))); // 31
 
-                    j++;
-                } while (j < length - 1);
+                    j++; // 32
+                } while (j < length - 1); // 33
             }
 
-            result.add(word.toString());
-            word.setLength(0);
+            result.add(word.toString()); // 34
+            word.setLength(0); // 35
         }
 
-        return result;
+        return result; // 36
     }
 }
