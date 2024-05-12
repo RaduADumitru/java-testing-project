e829b025ebed487c2ad8b8e48b498f02f53058d5
RaduADumitru <radu.dumitru@rocketmail.com>
2024-05-12 13:28:40 +0300

    Rename variables

--- src/main/java/com/company/CreditGranting.java
+++ src/main/java/com/company/CreditGranting.java
@@ -14,17 +14,29 @@ public class CreditGranting {
                                                 boolean doesLevelOfIndebtednessMatchesBankCriterion,
                                                 boolean doesCustomerReceiveItsIncomeInOurBank) {
         // mai jos sunt utilizate simbolurile de pe graf pentru a fi mai usor de urmarit codul
-        boolean condition1 = isCustomerEmployed && isCustomerWorkingForAtLeast6Months;
-        boolean condition2 = condition1 || isCustomerRetired;
-        boolean condition3 = !doesCustomerHaveBadCreditRecords;
-        boolean condition4 = condition3 && doesLevelOfIndebtednessMatchesBankCriterion;
-        boolean condition5 = condition2 && condition4;
-        boolean condition6 = condition5 && doesCustomerReceiveItsIncomeInOurBank;
 
-        if (!condition5)
+        // condition 1
+        boolean employedFor6Months = isCustomerEmployed && isCustomerWorkingForAtLeast6Months;
+
+        // condition 2
+        boolean employedOrRetired = employedFor6Months || isCustomerRetired;
+
+        // condition 3
+        boolean hasGoodCreditRecord = !doesCustomerHaveBadCreditRecords;
+
+        // condition 4
+        boolean meetsDebtCriteria = hasGoodCreditRecord && doesLevelOfIndebtednessMatchesBankCriterion;
+
+        // condition 5
+        boolean meetsEmploymentAndDebtCriteria = employedOrRetired && meetsDebtCriteria;
+
+        // condition 6
+        boolean reduceInterestRate = meetsEmploymentAndDebtCriteria && doesCustomerReceiveItsIncomeInOurBank;
+
+        if (!meetsEmploymentAndDebtCriteria)
             return BankResponse.CREDIT_NOT_APPROVED;
 
-        if (condition6)
+        if (reduceInterestRate)
             return BankResponse.CREDIT_APPROVED_WITH_REDUCED_INTEREST_RATE;
 
         return BankResponse.CREDIT_APPROVED;
